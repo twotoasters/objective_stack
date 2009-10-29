e8f4cdf08feaca2038810d65b7ca00d9fffa9be6
Blake Watters <blake@objective3.com>
2009-10-29 16:18:15 -0400

    Fix double init of Growl

--- objective_stack.rb
+++ objective_stack.rb
@@ -93,7 +93,6 @@ config.after_initialize do
   Bullet.alert = true
   Bullet.bullet_logger = true  
   Bullet.console = true
-  Bullet.growl = true
   Bullet.rails_logger = true
   Bullet.disable_browser_cache = true
 end
